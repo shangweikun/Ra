01bfb5e6611bec4078c7dae9ce845a2f35d73f5b
shangweikun <48422047+shangweikun@users.noreply.github.com>
2021-09-18 18:46:03 +0800

    Update hrs_person_info.sh 使用 getopt 将 变量参数化

--- shell/InteractiveFileFramework/hrs_person_info.sh
+++ shell/InteractiveFileFramework/hrs_person_info.sh
@@ -1,57 +1,14 @@
 #!/bin/sh
 
-#日期
-DATE=$(date +'%Y%m%d')
-
-PATH_ROOT="/appshare/attachment"
-PATH_FUNCTION_DIR="$PATH_ROOT/person"
-
-#SHELL
-PATH_SHELL="/dbshare/shell/file_batch"
-
-#IFT
-PATH_IFT="$PATH_ROOT/ift_data"
-PATH_DATA_DIR="$PATH_IFT/receive/person"
-
-#BACKUP
-PATH_BACK_DIR="$PATH_FUNCTION_DIR/backup"
-
-#LOG
-PATH_LOG_DIR="$PATH_FUNCTION_DIR/log"
-PATH_LOG_FILE="$PATH_LOG_DIR/hrs_person_$DATE.log"
-
-#数据库
-DB_USERID="$DBUSER/$DBPASSWD"
-DB_CONTROL_FILE="$PATH_SHELL/hrs_person_info.ctl"
-DB_SQLLDR_LOG_FILE="$PATH_LOG_DIR/hrs_person_sql_loader_$DATE.log"
-DB_SQLLDR_BAD_FILE="$PATH_LOG_DIR/hrs_person_back_up_$DATE.dat"
-
-#DATA
-HRS_PERSON_FILE_DIR=$PATH_DATA_DIR/$DATE
-HRS_PERSON_FILE_NAME_PREFIX="HRS_EBDC_"
-HRS_PERSON_FILE_NAME_SUFFIX="_001"
-HRS_PERSON_TASK_TITLE="HRS person info"
-
-#env
-#dev
-#PATH_IFT="/d/tmp/shellWorkspace"
-#HRS_PERSON_FILE_DIR=$PATH_IFT/$DATE
-#test=dev
-#PATH_IFT="/share_file/shangweikun/hrs_person_test"
-#HRS_PERSON_FILE_DIR=$PATH_IFT/$DATE
-#DB_CONTROL_FILE="$PATH_IFT/hrs_person_info.ctl"
-#DB_SQLLDR_LOG_FILE="$PATH_IFT/hrs_person_sql_loader.log"
-#DB_SQLLDR_BAD_FILE="$PATH_IFT/hrs_person_back_up.txt"
-
-echo "$(date +'%Y-%m-%d %H:%M:%S'): File start to parse [title: $HRS_PERSON_TASK_TITLE, date: $DATE]" >> $PATH_LOG_FILE
-
-sqlplus -s "$DB_USERID" <<EOF
+startParseFileSql (){
+  echo "$(date +'%Y-%m-%d %H:%M:%S'): File start to parse [title: $TASK_TITLE, date: $DATE]" >> $PATH_LOG_FILE
+  sqlplus -s "$DB_USERID" <<EOF
 set echo off feedback off heading off underline off;
 merge INTO EBDCNEW.DB_SHELL_TASK_INFO A
 using (
     select nvl((select task_id
                 from EBDCNEW.DB_SHELL_TASK_INFO B
-                where B.TASK_TITLE = to_char('$HRS_PERSON_TASK_TITLE')
+                where B.TASK_TITLE = to_char('$TASK_TITLE')
                   and B.TASK_DATE = to_char('$DATE')
                ), 0) as TASK_ID
     from dual
@@ -64,105 +21,260 @@ when not matched then
             TASK_BEGIN_TIME,
             STATUS,
             STATUS_TXT)
-    values (SEQ_DSTI_TASK_ID.nextval, to_char('$HRS_PERSON_TASK_TITLE'), to_char('$DATE'),
+    values (SEQ_DSTI_TASK_ID.nextval, to_char('$TASK_TITLE'), to_char('$DATE'),
             sysdate, '00', 'task start;')
 /
 commit
 /
 EOF
+}
 
-taskId=$(sqlplus -s "$DB_USERID" <<EOF
-set echo off feedback off heading off underline off;
-select task_id from DB_SHELL_TASK_INFO where TASK_TITLE='$HRS_PERSON_TASK_TITLE' and TASK_DATE='$DATE'
+getTaskId(){
+  id=$(sqlplus -s "$DB_USERID" <<EOF
+set head off
+set linesize 20000
+set echo off
+set feedback off
+set pagesize 0
+set termout off
+set trimout on
+set trimspool on
+select task_id from DB_SHELL_TASK_INFO where TASK_TITLE='$TASK_TITLE' and TASK_DATE='$DATE'
 /
 exit
 /
 EOF
 )
+  echo "$(date +'%Y-%m-%d %H:%M:%S'): taskId is $id" >> $PATH_LOG_FILE
+  return $id
+}
 
-echo "$(date +'%Y-%m-%d %H:%M:%S'): taskId is $taskId" >> $PATH_LOG_FILE
-
-if  ! find $PATH_DATA_DIR -ctime 0 -name "$HRS_PERSON_FILE_NAME_PREFIX$DATE$HRS_PERSON_FILE_NAME_SUFFIX.tar"  ; then
-    echo "$HRS_PERSON_FILE_NAME_PREFIX$DATE$HRS_PERSON_FILE_NAME_SUFFIX.tar file do not find, then exit" >> $PATH_LOG_FILE
-    exit 0
-fi
-
-if  [ ! 1 -eq $(find $PATH_DATA_DIR -ctime 0 -name "$HRS_PERSON_FILE_NAME_PREFIX$DATE$HRS_PERSON_FILE_NAME_SUFFIX.tar" | wc -l ) ] ; then
-    echo "$HRS_PERSON_FILE_NAME_PREFIX$DATE$HRS_PERSON_FILE_NAME_SUFFIX.tar file has find multiple files, then exit" >> $PATH_LOG_FILE
-    exit 0
-fi
-
-FILE_NAME=$(find $PATH_DATA_DIR -ctime 0 -name "$HRS_PERSON_FILE_NAME_PREFIX$DATE$HRS_PERSON_FILE_NAME_SUFFIX.tar")
-
-#判断文件夹并清理数据
-if [ ! -d $HRS_PERSON_FILE_DIR ] ; then
-    mkdir -p $HRS_PERSON_FILE_DIR
-    echo "$HRS_PERSON_FILE_DIR create success" >> $PATH_LOG_FILE
-else
-    cd $HRS_PERSON_FILE_DIR || exit 0
-    rm -rf ./*.txt
-    rm -rf ./*.jpg
-    echo "$HRS_PERSON_FILE_DIR all files clear success" >> $PATH_LOG_FILE
-fi
-
-#backup the file
-tar xvf $FILE_NAME --directory=$HRS_PERSON_FILE_DIR >> $PATH_LOG_FILE
-
-if  ! find $HRS_PERSON_FILE_DIR -ctime 0 -name "$HRS_PERSON_FILE_NAME_PREFIX$DATE.txt"  ; then
-    echo "$HRS_PERSON_FILE_NAME_PREFIX$DATE.txt file do not find, then exit" >> $PATH_LOG_FILE
-    exit 0
-fi
-
-if  [ ! 1 -eq $(find $HRS_PERSON_FILE_DIR -ctime 0 -name "$HRS_PERSON_FILE_NAME_PREFIX$DATE.txt" | wc -l ) ] ; then
-    echo "$HRS_PERSON_FILE_NAME_PREFIX$DATE.txt file has find multiple files, then exit" >> $PATH_LOG_FILE
-    exit 0
-fi
-
-FILE_NAME=$(find $HRS_PERSON_FILE_DIR -ctime 0 -name "$HRS_PERSON_FILE_NAME_PREFIX$DATE.txt")
-
-echo "$(date +'%Y-%m-%d %H:%M:%S'): $FILE_NAME unzip success" >> $PATH_LOG_FILE
-
-echo "$(date +'%Y-%m-%d %H:%M:%S'): File get $FILE_NAME success" >> $PATH_LOG_FILE
-sqlplus -s "$DB_USERID" <<EOF
-        set echo off feedback off heading off underline off;
-        update DB_SHELL_TASK_INFO set status='01',
-                                status_txt='get file success;'
-        where TASK_ID='$taskId'
-        /
-        commit
-        /
+getSwitch (){
+  switch=$(sqlplus -s "$DB_USERID" <<EOF
+set head off
+set linesize 20000
+set echo off
+set feedback off
+set pagesize 0
+set termout off
+set trimout on
+set trimspool on
+select PARAMVALUE from FBAS_SYSPM where PARAMNAME='HRS_OK_FILE_CHECK_SWITCH'
+/
+exit
+/
 EOF
+  )
+  return $switch
+}
 
-echo "$(date +'%Y-%m-%d %H:%M:%S'): DataBase SqlLoader Start load data" >> $PATH_LOG_FILE
-sqlldr userid="$DB_USERID" control="$DB_CONTROL_FILE" data="$FILE_NAME" log="$DB_SQLLDR_LOG_FILE" bad="$DB_SQLLDR_BAD_FILE" >> $PATH_LOG_FILE
-
-if [ $? -eq 0 ] ; then
+getFileSuccessSql (){
+  echo "$(date +'%Y-%m-%d %H:%M:%S'): File get $FILE_NAME success" >> $PATH_LOG_FILE
   sqlplus -s "$DB_USERID" <<EOF
-        set echo off feedback off heading off underline off;
-        update DB_SHELL_TASK_INFO set status='02',
-                                status_txt='script:file load success;',
-                                SCRIPT_DEAL_END_TIME=sysdate,
-                                SCRIPT_DEAL_COST=to_char(sysdate-SCRIPT_DEAL_BEGIN_TIME)
-        where TASK_ID=$taskId
-        /
-        commit
-        /
+set echo off feedback off heading off underline off;
+update DB_SHELL_TASK_INFO set status='01',
+                        status_txt='get file success;'
+where TASK_ID='$taskId'
+/
+commit
+/
+EOF
+}
+
+scriptParseFileSuccessSql(){
+  sqlplus -s "$DB_USERID" <<EOF
+set echo off feedback off heading off underline off;
+update DB_SHELL_TASK_INFO set status='02',
+                        status_txt='script:file load success;',
+                        SCRIPT_DEAL_END_TIME=sysdate,
+                        SCRIPT_DEAL_COST=to_char(sysdate-SCRIPT_DEAL_BEGIN_TIME)
+where TASK_ID=$taskId
+/
+commit
+/
 EOF
   echo "$(date +'%Y-%m-%d %H:%M:%S'): DataBase SqlLoader has loader data success" >> $PATH_LOG_FILE
-else
+}
+
+scriptParseFileFailSql(){
   sqlplus -s "$DB_USERID" <<EOF
-        set echo off feedback off heading off underline off;
-        update DB_SHELL_TASK_INFO set status='92',
-                                status_txt='script:file load fail;',
-                                SCRIPT_DEAL_END_TIME=sysdate,
-                                SCRIPT_DEAL_COST=to_char(sysdate-SCRIPT_DEAL_BEGIN_TIME)
-        where TASK_ID=$taskId
-        /
-        commit
-        /
+set echo off feedback off heading off underline off;
+update DB_SHELL_TASK_INFO set status='92',
+                        status_txt='script:file load fail;',
+                        SCRIPT_DEAL_END_TIME=sysdate,
+                        SCRIPT_DEAL_COST=to_char(sysdate-SCRIPT_DEAL_BEGIN_TIME)
+where TASK_ID=$taskId
+/
+commit
+/
 EOF
   echo "$(date +'%Y-%m-%d %H:%M:%S'): DataBase SqlLoader has loader data fail" >> $PATH_LOG_FILE
-fi
+}
 
+checkIsTheTargetFileIsOk (){
+  
+  echo "$(date +'%Y-%m-%d %H:%M:%S'): checkIsTheTargetFileIsOk begin fileName is[$1] " >> $PATH_LOG_FILE
+  while true 
+  do
+    echo "$(date +'%Y-%m-%d %H:%M:%S'): check fileName is[$1] " >> $PATH_LOG_FILE
+    find $PATH_DATA_DIR -name "$1" >> $PATH_LOG_FILE
+    if [ 0 -lt $( find $PATH_DATA_DIR -name "$1" | wc -l ) ] ; then {
+      echo "$(date +'%Y-%m-%d %H:%M:%S'): file [$1] is found , ift send success " >> $PATH_LOG_FILE
+      break
+    }
+    fi
 
-mv $PATH_DATA_DIR/*.tar $PATH_BACK_DIR/.
+    if [ $(date +'%Y%m%d%H%M') -gt $SHELL_LOOP_END_TIME ] ; then {
+      echo "$(date +'%Y-%m-%d %H:%M:%S'): loop out time, break loop " >> $PATH_LOG_FILE
+      break
+    }
+    fi
+
+    getSwitch
+    switch=$?
+    if [ $switch -eq "0" ] ; then {
+      echo "$(date +'%Y-%m-%d %H:%M:%S'): switch is [$switch] , break loop" >> $PATH_LOG_FILE
+      break
+    }
+    fi
+    sleep 10
+  done
+}
+
+findFileName(){
+
+  if  [ ! 1 -eq $(find $PATH_IFT -ctime 0 -name "$1" | wc -l ) ] ; then
+      echo "$1 file has find nor or multiple files, then exit" >> $PATH_LOG_FILE
+      exit 0
+  fi
+  FILE_NAME=$(find $PATH_IFT -ctime 0 -name "$1")
+}
+
+backupAndClearHistoryFile(){
+
+  echo "$(date +'%Y-%m-%d %H:%M:%S'): history file clear" >> $PATH_LOG_FILE
+
+  #unzip files
+  find $PATH_DATA_DIR -name "$PRE_DATE" -type d >> $PATH_LOG_FILE
+  if [ 0 -lt $( find $PATH_DATA_DIR -name "$PRE_DATE" -type d | wc -l ) ] ; then {
+      echo "$(date +'%Y-%m-%d %H:%M:%S'): rm -rf $PATH_DATA_DIR/$PRE_DATE" >> $PATH_LOG_FILE
+      rm -rf $PATH_DATA_DIR/$PRE_DATE
+  }
+  fi
+
+  #tar file
+  find $PATH_DATA_DIR -name "$FILE_NAME_PREFIX$PRE_DATE$FILE_NAME_SUFFIX.tar" >> $PATH_LOG_FILE
+  if [ 0 -lt $( find $PATH_DATA_DIR -name "$FILE_NAME_PREFIX$PRE_DATE$FILE_NAME_SUFFIX.tar" | wc -l ) ] ; then {
+      echo "$(date +'%Y-%m-%d %H:%M:%S'): rm -rf $PATH_DATA_DIR/$FILE_NAME_PREFIX$PRE_DATE$FILE_NAME_SUFFIX.tar" >> $PATH_LOG_FILE
+      rm -rf $PATH_DATA_DIR/$FILE_NAME_PREFIX$PRE_DATE$FILE_NAME_SUFFIX.tar
+  }
+  fi
+
+  #log
+  find $PATH_LOG_DIR -name "hrs_person_$PRE_DATE.log" >> $PATH_LOG_FILE
+  if [ 0 -lt $( find $PATH_LOG_DIR -name "hrs_person_$PRE_DATE.log" | wc -l ) ] ; then {
+      echo "$(date +'%Y-%m-%d %H:%M:%S'): rm -rf $PATH_LOG_DIR/hrs_person_$PRE_DATE.log" >> $PATH_LOG_FILE
+      rm -rf $PATH_LOG_DIR/hrs_person_$PRE_DATE.log
+  }
+  fi
+
+  #sqlloader log
+  find $PATH_LOG_DIR -name "hrs_person_sql_loader_$PRE_DATE.log" >> $PATH_LOG_FILE
+  if [ 0 -lt $( find $PATH_LOG_DIR -name "hrs_person_sql_loader_$PRE_DATE.log" | wc -l ) ] ; then {
+      echo "$(date +'%Y-%m-%d %H:%M:%S'): rm -rf $PATH_LOG_DIR/hrs_person_sql_loader_$PRE_DATE.log" >> $PATH_LOG_FILE
+      rm -rf $PATH_LOG_DIR/hrs_person_sql_loader_$PRE_DATE.log
+  }
+  fi
+
+  #sqlloader bad file
+  find $PATH_LOG_DIR -name "hrs_person_back_up_$PRE_DATE.dat" >> $PATH_LOG_FILE
+  if [ 0 -lt $( find $PATH_LOG_DIR -name "hrs_person_back_up_$PRE_DATE.dat" | wc -l ) ] ; then {
+      echo "$(date +'%Y-%m-%d %H:%M:%S'): rm -rf $PATH_LOG_DIR/hrs_person_back_up_$PRE_DATE.dat" >> $PATH_LOG_FILE
+      rm -rf $PATH_LOG_DIR/hrs_person_back_up_$PRE_DATE.dat
+  }
+  fi
+}
+
+main(){
+
+  startParseFileSql
+
+  getTaskId
+
+  taskId=$?
+
+  checkIsTheTargetFileIsOk "$FILE_NAME_PREFIX$DATE$FILE_NAME_SUFFIX.xml" 
+
+  findFileName "$FILE_NAME_PREFIX$DATE$FILE_NAME_SUFFIX.tar"
+
+  if [ ! -d $FILE_DIR ] ; then
+      mkdir $FILE_DIR
+      echo "$FILE_DIR create success" >> $PATH_LOG_FILE
+  else
+      rm -rf $FILE_DIR/*.txt
+      rm -rf $FILE_DIR/*.jpg
+      echo "$FILE_DIR all files clear success" >> $PATH_LOG_FILE
+  fi
+
+  #backup the file
+  tar xvf $FILE_NAME --directory=$FILE_DIR >> $PATH_LOG_FILE
+
+  findFileName "$FILE_NAME_PREFIX$DATE.txt"
+
+  getFileSuccessSql
+
+  echo "$(date +'%Y-%m-%d %H:%M:%S'): DataBase SqlLoader Start load data" >> $PATH_LOG_FILE
+  sqlldr userid="$DB_USERID" control="$DB_CONTROL_FILE" data="$FILE_NAME" log="$DB_SQLLDR_LOG_FILE" bad="$DB_SQLLDR_BAD_FILE" >> $PATH_LOG_FILE
+
+  if [ $? -eq 0 ] ; then
+    scriptParseFileSuccessSql
+  else
+    scriptParseFileFailSql
+  fi
+
+  backupAndClearHistoryFile
+}
+
+#DATE
+DATE=$(date +'%Y%m%d')
+PRE_DATE=$(date -d "-7day" +'%Y%m%d')
+
+#LOOP-TIME
+SHELL_LOOP_END_TIME=$(date -d "1hour" +'%Y%m%d%H%M') 
+
+ARGS=`getopt -a -o a:b:c:d:e:f:g:h: --long FunctionPath:,ShellPath:,DataPath:,ControlFile:,SqlLoaderPreffix:,FileNamePreffix:,FileNameSuffix:,TaskTitle: -- "$@"`
+
+echo "$@"
+
+eval set -- "$ARGS"
+while true ; do
+        case "$1" in
+                -a|--FunctionPath) echo "Option FunctionPath $2";PATH_FUNCTION_DIR="$2";shift ;;
+                -b|--ShellPath) echo "Option ShellPath $2";PATH_SHELL="$2";shift ;;
+                -c|--DataPath) echo "Option DataPath $2";PATH_DATA_DIR="$2";shift ;;
+                -d|--ControlFile) echo "Option ControlFile $2";CONTROL_FILE="$2";shift ;;
+                -e|--SqlLoaderPreffix) echo "Option SqlLoaderPreffix $2";SQLLOADER_PREFFIX="$2";shift ;;
+                -f|--FileNamePreffix) echo "Option FileNamePreffix $2";FILE_NAME_PREFIX="$2";shift ;;
+                -g|--FileNameSuffix) echo "Option FileNameSuffix $2";FILE_NAME_SUFFIX="$2";shift ;;
+                -h|--TaskTitle) echo "Option TaskTitle $2";TASK_TITLE="$2";shift ;;
+                --) shift ; break ;;
+        esac
+shift
+done
+echo "Parase arguments"
+
+#LOG
+PATH_LOG_DIR="$PATH_FUNCTION_DIR/log"
+PATH_LOG_FILE="$PATH_LOG_DIR/$FILE_NAME_PREFIX$DATE.log"
+
+#DATABASE
+DB_USERID="$DBUSER/$DBPASSWD"
+DB_CONTROL_FILE="$PATH_SHELL/$CONTROL_FILE"
+DB_SQLLDR_LOG_FILE="$PATH_LOG_DIR/$SQLLOADER_PREFFIX$DATE.log"
+DB_SQLLDR_BAD_FILE="$PATH_LOG_DIR/$SQLLOADER_PREFFIX$DATE.dat"
+
+#DATA
+FILE_DIR=$PATH_DATA_DIR/$DATE
+
+main
+echo 0
