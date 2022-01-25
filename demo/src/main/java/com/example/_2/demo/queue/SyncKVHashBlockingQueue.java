0885df80d604e9df57d67391d77a7b2faab53132
shangweikun <48422047+shangweikun@users.noreply.github.com>
2022-01-25 20:41:04 +0800

    Update SyncKVHashBlockingQueue.java

--- demo/src/main/java/com/example/_2/demo/queue/SyncKVHashBlockingQueue.java
+++ demo/src/main/java/com/example/_2/demo/queue/SyncKVHashBlockingQueue.java
@@ -11,110 +11,110 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class SyncKVHashBlockingQueue<K, E extends RedirectKey<K>>
-		extends AbstractQueue<E> {
+        extends AbstractQueue<E> {
 
-	private final int pool_size;
-	private final ConcurrentHashMap<String, K> THREAD_LOCK = new ConcurrentHashMap<>();
-	private final ConcurrentHashMap<K, Entity<K, E>> QUEUE_CACHE = new ConcurrentHashMap<>();
-	private final BlockingQueue<E> queue;
+    private final int poolSize;
+    private final ConcurrentHashMap<String, K> THREAD_LOCK = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<K, Entity<K, E>> QUEUE_CACHE = new ConcurrentHashMap<>();
+    private final BlockingQueue<E> queue;
 
-	public SyncKVHashBlockingQueue(int pool_size, BlockingQueue<E> queue) {
-		this.pool_size = pool_size;
-		this.queue = queue;
-	}
+    public SyncKVHashBlockingQueue(int poolSize, BlockingQueue<E> queue) {
+        this.poolSize = poolSize;
+        this.queue = queue;
+    }
 
-	@Override
-	public Iterator<E> iterator() {
-		return null;
-	}
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
 
-	@Override
-	public int size() {
-		return 0;
-	}
+    @Override
+    public int size() {
+        return 0;
+    }
 
-	@AllArgsConstructor
-	private static class Entity<K, E> {
-		final Object LOCK = new Object();
-		K key;
-		LinkedList<E> list;
-	}
+    @AllArgsConstructor
+    private static class Entity<K, E> {
+        final Object lock = new Object();
+        K key;
+        LinkedList<E> list;
+    }
 
-	@Override
-	public E poll() {
-		String threadName = Thread.currentThread().getName();
-		K key;
-		if ((key = THREAD_LOCK.get(threadName)) != null) {
-			Entity<K, E> entity = QUEUE_CACHE.get(key);
-			nullPoint:
-			if (entity.list.getFirst() == null) {
-				synchronized (entity.LOCK) {
-					if (entity.list.getFirst() == null) {
-						THREAD_LOCK.remove(threadName);
-						break nullPoint;
-					}
-				}
-				return entity.list.pollFirst();
-			}
-		}
+    @Override
+    public E poll() {
+        String threadName = Thread.currentThread().getName();
+        K key;
+        if ((key = THREAD_LOCK.get(threadName)) != null) {
+            Entity<K, E> entity = QUEUE_CACHE.get(key);
+            nullPoint:
+            if (entity.list.getFirst() == null) {
+                synchronized (entity.lock) {
+                    if (entity.list.getFirst() == null) {
+                        THREAD_LOCK.remove(threadName);
+                        break nullPoint;
+                    }
+                }
+                return entity.list.pollFirst();
+            }
+        }
 
-		for (; ; ) {
-			E e = queue.poll();
-			if (e == null) {
-				continue;
-			}
+        for (; ; ) {
+            E e = queue.poll();
+            if (e == null) {
+                continue;
+            }
 
-			while (true) {
-				Entity<K, E> head = QUEUE_CACHE.get(e.key);
-				if (head == null) {
-					head = new Entity<>(e.key, ListUtil.toLinkedList(e));
-					for (; ; ) {
-						synchronized (this) {
-							Entity<K, E> checkKey = QUEUE_CACHE.get(e.key);
-							if (checkKey == null && THREAD_LOCK.size() < pool_size) {
-								THREAD_LOCK.put(threadName, head.key);
-								QUEUE_CACHE.put(head.key, head);
-								return head.list.pollFirst();
-							}
-						}
-					}
-				} else if (!THREAD_LOCK.containsValue(head.key)) {
-					synchronized (head.LOCK) {
-						if (!THREAD_LOCK.containsValue(head.key)) {
-							if (THREAD_LOCK.size() < pool_size) {
-								THREAD_LOCK.put(threadName, head.key);
-								head.list.addLast(e);
-								return head.list.pollFirst();
-							}
-						}
-					}
-				} else {
-					synchronized (head.LOCK) {
-						if (THREAD_LOCK.containsValue(head.key)) {
-							head.list.add(e);
-						}
-					}
-				}
-			}
-		}
-	}
+            while (true) {
+                Entity<K, E> head = QUEUE_CACHE.get(e.key);
+                if (head == null) {
+                    head = new Entity<>(e.key, ListUtil.toLinkedList(e));
+                    for (; ; ) {
+                        synchronized (this) {
+                            Entity<K, E> checkKey = QUEUE_CACHE.get(e.key);
+                            if (checkKey == null && THREAD_LOCK.size() < poolSize) {
+                                THREAD_LOCK.put(threadName, head.key);
+                                QUEUE_CACHE.put(head.key, head);
+                                return head.list.pollFirst();
+                            }
+                        }
+                    }
+                } else if (!THREAD_LOCK.containsValue(head.key)) {
+                    synchronized (head.lock) {
+                        if (!THREAD_LOCK.containsValue(head.key)) {
+                            if (THREAD_LOCK.size() < poolSize) {
+                                THREAD_LOCK.put(threadName, head.key);
+                                head.list.addLast(e);
+                                return head.list.pollFirst();
+                            }
+                        }
+                    }
+                } else {
+                    synchronized (head.lock) {
+                        if (THREAD_LOCK.containsValue(head.key)) {
+                            head.list.add(e);
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-	@Override
-	public E peek() {
-		return null;
-	}
+    @Override
+    public E peek() {
+        return null;
+    }
 
-	@Override
-	public boolean offer(@NotNull E e) {
-		Entity<K, E> entity;
-		while ((entity = QUEUE_CACHE.get(e.key)) != null) {
-			synchronized (entity.LOCK) {
-				if (THREAD_LOCK.containsValue(e.key)) {
-					entity.list.addLast(e);
-				}
-			}
-		}
-		return queue.add(e);
-	}
+    @Override
+    public boolean offer(@NotNull E e) {
+        Entity<K, E> entity;
+        while ((entity = QUEUE_CACHE.get(e.key)) != null) {
+            synchronized (entity.lock) {
+                if (THREAD_LOCK.containsValue(e.key)) {
+                    entity.list.addLast(e);
+                }
+            }
+        }
+        return queue.add(e);
+    }
 
 }
