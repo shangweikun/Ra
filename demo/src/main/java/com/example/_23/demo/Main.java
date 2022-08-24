8695b2ccee157bf0c53bc9040a03b4eba6143fd6
Hasaki <shangweikun@yeah.net>
2022-08-24 14:43:02 +0800

    20220824 commit

---
+++ demo/src/main/java/com/example/_23/demo/Main.java
@@ -0,0 +1,25 @@
+package com.example._23.demo;
+
+import io.undertow.Undertow;
+import io.undertow.util.Headers;
+import io.undertow.util.HttpString;
+
+public class Main {
+
+
+    private static class UndertowDemo {
+
+        public static void main(String[] args) {
+
+            Undertow server = Undertow.builder()
+                    .addHttpListener(8080, "localhost")
+                    .setHandler(exchange -> {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello World");
+                        exchange.getResponseHeaders().put(new HttpString("count"), "1");
+                    }).build();
+            server.start();
+        }
+    }
+
+}
